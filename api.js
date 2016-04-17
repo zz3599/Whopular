f0b0b3bf08c1a8d3fc786cd9b9e55ead67086162
Brook Zhou <zz3599@yahoo.com>
2016-04-17 01:56:49 -0700

    Refactor, twitter API

---
+++ api.js
@@ -0,0 +1,160 @@
+var request = require('request');
+
+//TODO: move to DB
+var secrets = {
+	API_KEY : "ee400c33-906c-49b6-9515-28a518446c42",
+	TWITTER_ACCESS_TOKEN : ""
+}
+
+module.exports = {
+	/////////////////////////////////////////////
+	////Get request for the entities in the article
+	/////////////////////////////////////////////
+	getEntities: function (url, entity_type, callback) {
+		request({
+			uri: "https://api.havenondemand.com/1/api/async/extractentities/v2",
+		  	method: "POST",
+		  	timeout: 60000,
+		  	form: {
+		    	apikey: secrets.API_KEY,
+		    	url: url,
+		    	entity_type: entity_type
+		  	}
+		}, function(error, response, body) {
+			body = JSON.parse(body);
+		  	console.log(body.jobID);	
+		  	module.exports.awaitAsync(body.jobID, callback);
+		});
+	}, 
+
+	// General call that should be invoked from all async get/post callbacks
+	awaitAsync: function (jobID, callback){	
+		console.log("Making request to " + "https://api.havenondemand.com/1/job/status/" + jobID + "?apikey=" + secrets.API_KEY);
+		request({
+		  	uri: "https://api.havenondemand.com/1/job/status/" + jobID + "?apikey=" + secrets.API_KEY,
+			method: "GET",
+			timeout: 10000,
+		}, function(error, response, body) {
+			body = JSON.parse(body);
+			var status = body.status;
+			if (status === 'failed'){
+				console.log("Request failed");
+			} else if (status === 'finished'){
+				console.log("Request completed " + jobID);
+				callback(body.actions[0].result);
+			} else {
+				module.exports.awaitAsync(jobID, callback);
+			}
+		});	
+	},
+
+	// Callback for getEntities
+	getEntitiesDone: function (result) {
+		console.log("Get entities done=" + result);
+		//ignore names that only have 1 match
+		var entities = result.entities.filter(function (entity){ return entity.matches.length > 1; });
+		for(var i = 0; i < entities.length; i++){
+			console.log("Found entity: " + entities[i].normalized_text + ", picture: " + entities[i].additional_information.image);
+		}
+	},
+
+	/////////////////////////////////////////////
+	////Get request for the sentiments of the specified string. 
+	////Can be an aggregate of multiple sentences
+	/////////////////////////////////////////////
+	getSentiments: function (text, language, callback) {
+		console.log("Making request to get sentiments for " + text);
+		request({
+			uri: "https://api.havenondemand.com/1/api/async/analyzesentiment/v1",
+		  	method: "POST",
+		  	timeout: 60000,
+		  	form: {
+		    	apikey: secrets.API_KEY,
+		    	text: text,
+		    	language: language
+		  	}
+		}, function(error, response, body) {
+			body = JSON.parse(body);
+		  	console.log(body.jobID);	
+		  	module.exports.awaitAsync(body.jobID, callback);
+		});
+	},
+
+	// Callback for getsentiments
+	getSentimentsDone: function (result) {
+		console.log("Get sentiments done=" + result);
+		var numPositive = result.positive.length;
+		var numNegative = result.negative.length;
+		var aggregateScore = result.aggregate.score;
+		var aggregateSentiment = result.aggregate.sentiment;
+		console.log("Total negative: " + numNegative + ", positive: " + numPositive + ", aggregateScore: " + aggregateScore + ", aggregateSentiment: " + aggregateSentiment);
+	},
+
+	// Should only be called once
+	initializeTwitter: function (callback) {
+		var key = "Lt0Y4gU3um7ou8WQdQrPjcefw";
+		//TODO: move to DB
+		var secret = "xRMUKxd9Srl7PHxICidbSpObIrf2RCbOxeWfNW3b4wTiuAWNgd";
+		var encoded = new Buffer(key + ":" + secret).toString('base64');
+		console.log(encoded);
+		request({
+			uri: "https://api.twitter.com/oauth2/token",
+		  	method: "POST",
+		  	timeout: 10000,
+		  	headers: {
+		  		"Authorization" : "Basic " + encoded,
+		  		"Content-Type": "application/x-www-form-urlencoded;charset=UTF-8"
+		  	},
+		  	form: {
+		    	"grant_type" : "client_credentials"
+		  	}
+		}, function(error, response, body) {
+			console.log(body);	
+			body = JSON.parse(body);
+			if (body.token_type === 'bearer') {
+				secrets.TWITTER_ACCESS_TOKEN = body["access_token"];
+			}
+			callback();
+		});
+	},
+
+	/////////////////////////////////////////////
+	////Gets the tweets for one entity
+	/////////////////////////////////////////////
+	getTweetsForEntity: function (entityName, callback) {
+		if (entityName === null || entityName.length === 0) {
+			console.log("Empty entityname");
+			callback("");
+		}
+		if (secrets.TWITTER_ACCESS_TOKEN === null || secrets.TWITTER_ACCESS_TOKEN.length === 0){
+			console.log("No twitter access token");
+			callback("");
+		}
+		// Put OR's around everything so we can get everything in one search
+		var allEntities = entityName.split(" ").join(" OR ");
+		var queryString = "q=" + encodeURIComponent(allEntities) + "&result_type=recent&count=1000&lang=en";
+		console.log("Twitter Search query: " + queryString);
+		request({
+		  	uri: "https://api.twitter.com/1.1/search/tweets.json?" + queryString,
+			method: "GET",
+			timeout: 10000,
+			headers: {
+		  		"Authorization" : "Bearer " + secrets.TWITTER_ACCESS_TOKEN,
+		  		"Content-Type": "application/x-www-form-urlencoded;charset=UTF-8"
+		  	},
+		}, function(error, response, body) {
+			body = JSON.parse(body);
+			console.log("Tweet count received: " + body.statuses.length);
+			//Concatentate all the tweets into one string, separated by periods, but not extra periods
+			//since that would screw up the sentiment analysis due to extra sentences
+			var tweetText = "";
+			for (var i = 0; i < body.statuses.length; i++) {
+				if (tweetText.length > 0 && tweetText[tweetText.length-1] !== '.'){
+					tweetText += '.';
+				}
+				tweetText += body.statuses[i].text.trim();
+			}
+			callback(tweetText);
+		});	
+	}
+};
--- server.js
+++ server.js
@@ -2,13 +2,9 @@ var express = require('express');
 var path = require('path');
 var bodyparser = require('body-parser');
 var cookieSession = require('express-session');
-var request = require('request');
+
 //hook up all the routers here
 var app = express();
-
-//Yes, key is public for everyone.
-
-var API_KEY = "ee400c33-906c-49b6-9515-28a518446c42";
 
 //Assets all in public folder
 app.use('/public', express.static(__dirname + '/public'));
@@ -20,62 +16,18 @@ app.use(cookieSession({
 }));
 app.locals.pretty = true;
 
-/////////////////////////////////////////////
-//Get request for the entities in the article
-/////////////////////////////////////////////
-function getEntities(url, entity_type, callback) {
-	request({
-		uri: "https://api.havenondemand.com/1/api/async/extractentities/v2",
-	  	method: "POST",
-	  	timeout: 60000,
-	  	form: {
-	    	apikey: API_KEY,
-	    	url: url,
-	    	entity_type: entity_type
-	  	}
-	}, function(error, response, body) {
-		body = JSON.parse(body);
-	  	console.log(body.jobID);	
-	  	awaitAsync(body.jobID, callback);
-	  	//getEntitiesDone(body);
-	});
-}
+// API for invoking HP, Twitter, etc.
+var api = require('./api.js');
 
-// General call that should be invoked from all async get/post callbacks
-function awaitAsync(jobID, callback){	
-	console.log("Making request to " + "https://api.havenondemand.com/1/job/status/" + jobID + "?apikey=" + API_KEY);
-	request({
-	  	uri: "https://api.havenondemand.com/1/job/status/" + jobID + "?apikey=" + API_KEY,
-		method: "GET",
-		timeout: 10000,
-	}, function(error, response, body) {
-		body = JSON.parse(body);
-		var status = body.status;
-		if (status === 'failed'){
-			console.log("Request failed");
-		} else if (status === 'finished'){
-			console.log("Request completed");
-			callback(body.actions[0].result);
-		} else {
-			//TODO: maybe stack overflow
-			awaitAsync(jobID, callback);
-		}
-	});	
-}
 
-// Extract the entities that have more than 1 match
-function getEntitiesDone(result) {
-	console.log("Get entities done=" + result);
-	//ignore names that only have 1 match
-	var entities = result.entities.filter(function (entity){ return entity.matches.length > 1; });
-	for(var i = 0; i < entities.length; i++){
-		console.log("Found entity: " + entities[i].normalized_text + ", picture: " + entities[i].additional_information.image);
+//api.getEntities("http://espn.go.com/nba/story/_/id/15202045/nba-kobe-bryant-fitting-farewell", "people_eng", getEntitiesDone);
+//api.getSentiments("That was terrible. He's really the best when nothing matters.", "eng", getSentimentsDone);
+api.initializeTwitter(
+	function(){
+		api.getTweetsForEntity("Kobe Bryant", function(tweets) {
+			api.getSentiments(tweets, "eng", api.getSentimentsDone);
+		});
 	}
-}
-
-getEntities("http://espn.go.com/nba/story/_/id/15202045/nba-kobe-bryant-fitting-farewell", "people_eng", getEntitiesDone);
-
-
-
+);
 
 app.listen(3000);
